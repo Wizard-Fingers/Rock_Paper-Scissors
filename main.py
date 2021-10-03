5352abea04e773bcbf7e8a4a3c9734ccf3f2afbe
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-10-03 20:06:30 +0000

    put the code in a while loop and made a score chart

--- main.py
+++ main.py
@@ -1,39 +1,58 @@
 from random import randint
-#v.3 Rock-Papper-Scissors project
-print("Rock!")
-print("Paper!")
-print("Scissors!")
-P1 = input("Make a move: ").lower()
+player = 0
+computer = 0
+
+while player < 2 and computer <2:
+#v.4 Rock-Papper-Scissors project
+  print(f"Your Score: {player}")
+  print(f"Computer Score: {computer}")
+  print("Rock!")
+  print("Paper!")
+  print("Scissors!")
+  P1 = input("Make a move: ").lower()
 #Random selection based on import random
-P2 = randint(0,2)
-if P2 == 0:
-  P2 = "rock"
-elif P2 == 1:
-  P2 = "paper"
-else:  
-  P2 = "scissors"
+  P2 = randint(0,2)
+  if P2 == 0:
+    P2 = "rock"
+  elif P2 == 1:
+    P2 = "paper"
+  else:  
+    P2 = "scissors"
 #Game logic:
 #tie
-if P1 == P2:
-  print("Its a tie!") 
+  if P1 == P2:
+    print("Its a tie!") 
 #rock
-elif P1 == "rock":
-  if P2 == "scissors":
-    print("You win")
-  elif P2 == "paper":
-    print("Computer wins")
-#paper
-elif P1 == "paper":
-  if P2 == "rock":
-    print("You win")
-  elif P2 == "scissors":   
+  elif P1 == "rock":
+    if P2 == "scissors":
+      print("You win")
+      player += 1
+    elif P2 == "paper":
       print("Computer wins")
+      computer += 1
+#paper
+  elif P1 == "paper":
+    if P2 == "rock":
+      print("You win")
+      player += 1
+    elif P2 == "scissors":   
+        print("Computer wins")
+        computer += 1
 #scissors
-elif P1 == "scissors":
-  if P2 == "paper":
-    print("You win")
-  elif P2 == "rock":
-    print("Computer wins")
+  elif P1 == "scissors":
+    if P2 == "paper":
+      print("You win")
+      player += 1
+    elif P2 == "rock":
+      print("Computer wins")
+      computer += 1
 #in case of inpropper input
+  else:
+    print("something went wrong...")
+
+if player > computer:
+  print("Congrats, you win!")
 else:
-  print("something went wrong...")
+  print("O no you lost")
+
+final_score = print(f"Your Score: {player} Computer Score: {computer}")
