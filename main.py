056de6bf5bcbdf67d01b2d22afc78cc41293288d
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-10-03 14:32:32 +0000

    I changed the Player2 to use random

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-#v.2 Rock-Papper-Scissors project
+from random import randint
+#v.3 Rock-Papper-Scissors project
 print("Rock!")
 print("Paper!")
 print("Scissors!")
-P1 = input("Player 1, Make a move: ").lower()
-P2 = input("Player 2, Make a move: ").lower()
+P1 = input("Make a move: ").lower()
+#Random selection based on import random
+P2 = randint(0,2)
+if P2 == 0:
+  P2 = "rock"
+elif P2 == 1:
+  P2 = "paper"
+else:  
+  P2 = "scissors"
 #Game logic:
 #tie
 if P1 == P2:
@@ -11,21 +19,21 @@ if P1 == P2:
 #rock
 elif P1 == "rock":
   if P2 == "scissors":
-    print("player1 wins")
+    print("You win")
   elif P2 == "paper":
-    print("player2 wins")
+    print("Computer wins")
 #paper
 elif P1 == "paper":
   if P2 == "rock":
-    print("Player1 wins")
+    print("You win")
   elif P2 == "scissors":   
-      print("Player2 wins")
+      print("Computer wins")
 #scissors
 elif P1 == "scissors":
   if P2 == "paper":
-    print("Player1 wins")
+    print("You win")
   elif P2 == "rock":
-    print("Player2 wins")
+    print("Computer wins")
 #in case of inpropper input
 else:
   print("something went wrong...")
